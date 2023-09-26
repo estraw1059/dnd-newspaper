53ff8dd76876f1e706c5ea0feacbffbb74578684
Eric <estraw@smu.edu>
2023-09-25 20:44:51 -0500

    Setup basic Toolbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <nav className="bg-blue-500 p-4">
+        <div className="container mx-auto">
+            <div className="flex justify-between items-center">
+                <a href="#" className="text-white text-2xl font-semibold">My Website</a>
+                <ul className="flex space-x-4">
+                    <li><a href="#" className="text-white hover:text-gray-300">Home</a></li>
+                    <li><a href="#" className="text-white hover:text-gray-300">About</a></li>
+                    <li><a href="#" className="text-white hover:text-gray-300">Services</a></li>
+                    <li><a href="#" className="text-white hover:text-gray-300">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        {children}
+      </body>
     </html>
   )
 }
