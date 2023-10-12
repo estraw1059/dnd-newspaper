2ab5d4af5e654a6041f485b41419765c8b3de96d
Eric <estraw@smu.edu>
2023-10-12 06:30:08 -0500

    Added some layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import NewsArticle from "@/components/NewsArticle";
+import NewsPaperFullPageOnline from "@/components/NewsPaperFullPageOnline";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <NewsArticle text='Basic New article with some text' title='Dragon Fights Spotted'/>
-      </div>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    //   <div>
+    //     <NewsArticle text='Basic New article with some text' title='Dragon Fights Spotted'/>
+    //   </div>
+    // </main>
+    <main>
+      <NewsPaperFullPageOnline/>
     </main>
-  )
+  );
 }
---
+++ src/components/NewsPaperFullPageOnline.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './NewsPaperOnlineArticle.css'
+const NewsPaperFullPageOnline = () => {
+    return (
+        <div className="container">
+            <div className="frontpage">
+                <div className="fp-cell fp-cell--1">
+                <div className="fp-item">1</div>
+                </div>
+                <div className="fp-cell fp-cell--2">
+                <div className="fp-item">2</div>
+                </div>
+                <div className="fp-cell fp-cell--3">
+                <div className="fp-item">3</div>
+                </div>
+                <div className="fp-cell fp-cell--4">
+                <div className="fp-item">4</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NewsPaperFullPageOnline;
