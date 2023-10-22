6867ef81ecc4aa537640c08d3a11cde6a38c09fb
Eric <estraw@smu.edu>
2023-10-22 00:04:47 -0500

    Adjust css to fix margin issues

--- src/components/NewsArticle.tsx
+++ src/components/NewsArticle.tsx
@@ -9,8 +9,8 @@ type articleProps = {
 const NewsArticle = (props: articleProps) => {
     const { title, text } = props;
     return (
-        <div className='textBox'>
-            <h1 className='text-3xl font-bold text-center mb-4'>{title}</h1>
+        <div className='textBox '>
+            <h1 className='text-3xl font-bold text-center my-4'>{title}</h1>
             <div className='text-gray-700 text-justify'>{text}</div>    
         </div>
     );
