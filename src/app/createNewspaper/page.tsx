fc38349d6f853a6c70e4997cfa2eb21e373dc10a
Eric <estraw@smu.edu>
2023-11-19 20:33:53 -0600

    Setup Create newspaper page

---
+++ src/app/createNewspaper/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            This is where I will create a new newspaper
+        </div>
+    );
+};
+
+export default page;
