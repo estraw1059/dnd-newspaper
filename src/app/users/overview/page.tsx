fcc621a1eb72a4d8bc936f64fece3ac79f715639
Eric <estraw@smu.edu>
2024-01-27 13:01:09 -0600

    Minor build fix

--- src/app/users/overview/page.tsx
+++ src/app/users/overview/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { auth, db } from '../../../firebase';
 import { useRouter } from 'next/navigation'
 
-const page = () => {
+const Page = () => {
     const router = useRouter();
 
     auth.onAuthStateChanged(user => {
@@ -18,4 +18,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default Page;
