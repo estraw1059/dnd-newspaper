cc77aff06ad464a4856882129936d3a82b047f5c
unknown <estraw@smu.edu>
2024-03-17 18:37:10 -0500

    Basic Clean-up

--- src/firebaseAdmin.tsx
+++
@@ -1,9 +0,0 @@
-import { getAuth } from "firebase/auth";
-
-var admin = require("firebase-admin");
-var serviceAccount = require("../adminAPIKey.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-},
-"serverApp");
-export const auth = getAuth();
