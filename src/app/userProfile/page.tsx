536d2f02f4c51279b0bbff704cfd370bd1cb99d9
Eric <estraw@smu.edu>
2024-07-01 17:08:55 -0500

    Provide additional user info on the user page

--- src/app/userProfile/page.tsx
+++ src/app/userProfile/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { getCookie } from "cookies-next";
 import { db }  from "../../firebase";
-import { query, where, collection, getDocs, DocumentData } from "firebase/firestore";
+import { query, where, collection, getDocs, DocumentData, doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation';
 import UserArticleCard from "@/components/UserArticleCard";
@@ -13,15 +13,46 @@ type UserArticle = {
   subdomain: string;
   title: string;
   uid: string;
-}
+};
+
+type UserInfo = {
+  firstName: string;
+  lastName: string;
+  subdomain: string;
+};
 
 
 export default function Page() {
 
   const [userArticles, setUserArticles] = useState<UserArticle[]>([]);
+  const [userInfo, setUserInfo] = useState<UserInfo>();
   const router = useRouter();
 
   useEffect(() => {
+
+    const getUserInfo = async () => {
+      const userUID = getCookie("userUID");
+      if (!userUID) {
+        // redirect to login page
+        router.push("/login");
+        return;
+      }
+      const userDoc = doc(db, 'userInfo', userUID as string);
+      const docSnap = await getDoc(userDoc);
+      if (docSnap.exists()) {
+        const docData = docSnap.data();
+        setUserInfo({
+          firstName: docData?.firstName,
+          lastName: docData?.lastName,
+          subdomain: docData?.subdomain
+        });
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No User Found");
+        router.push("/login");
+      }
+  }
+
     const getUserNewspapers = async () => {
       const userUID = getCookie("userUID");
       if (!userUID) {
@@ -39,15 +70,34 @@ export default function Page() {
     }
 
 
-    getUserNewspapers()
+    getUserNewspapers();
+    getUserInfo();
   }, []);
 
   return (
-    <Container className="flex flex-col items-center bg-white h-screen p-10">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full">
-        {userArticles.map((article, key) => {
-          return (<UserArticleCard key={key} newsPaperTitle={article.title} password={article.password} createdDate={article.createdDate} subdomain={article.subdomain} />)
-        })}
+    <Container className="flex flex-col items-center bg-white h-screen p-10 text-black">
+      <div className="w-full m-3">
+        <h1 className="text-3xl font-bold p-3">User Profile</h1>
+        <p className="text-lg p-3">Welcome to your profile page. Here you can see all the articles you have created.</p>
+          {/* Add Username Field */}
+        <div className="p-3">
+          <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+          <span className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm max-w-[250px]">{`${userInfo?.firstName} ${userInfo?.lastName}`}</span>
+        </div>
+
+        {/* Add Subdomain Field */}
+        <div className="p-3">
+          <label htmlFor="subdomain" className="block text-sm font-medium text-gray-700">Subdomain</label>
+          <span className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm max-w-[250px]">/{userInfo?.subdomain}</span>
+        </div>
+      </div>
+      <div className="w-full m-3">
+        <h1 className="text-3xl font-bold p-3">Your Articles</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full">
+          {userArticles.map((article, key) => {
+            return (<UserArticleCard key={key} newsPaperTitle={article.title} password={article.password} createdDate={article.createdDate} subdomain={article.subdomain} />)
+          })}
+        </div>
       </div>
     </Container>
 
